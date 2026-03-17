0c55404a31697bd278ae8bf37e1c69a90a30650a
Kekeoliver <kethelyn.oliver.rocha@gmail.com>
2026-03-17 11:45:25 -0300

    aula02

---
+++ auka02-variáveis/aula01A-py.py
@@ -0,0 +1,29 @@
+print("olá mundo")
+
+print(7 + 4)
+print("7 + 4")
+print("7" + "4") #CONCATENAÇÃO DE TRINGS
+
+#Comentários de 1 linha
+
+
+ 
+#VARIÁVEIS
+nome="kethelyn"
+idade= 17
+peso= 70.2
+
+print("oii", nome, idade, peso)
+print("oii, {nome}!")
+
+''''# INPUT - SIMULA FORMS NO CMD
+nome = input("Digite seu nome: ")
+idade = int(input("digite a sua idade: ")) #com int retorna dados
+peso = input("digite o seu peso: ")
+
+print(nome, idade, peso)'''''
+
+# desafio 1
+nome= input("digite seu nome: ")
+print("seja muito bem-vindo " + nome)
+
