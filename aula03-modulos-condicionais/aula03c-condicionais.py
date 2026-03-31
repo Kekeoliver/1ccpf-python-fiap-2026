e390d6c5a29e6a7ad637a8e2fdc89b691cfd9e50
Kekeoliver <kethelyn.oliver.rocha@gmail.com>
2026-03-31 11:42:22 -0300

    aula 3 - modulos, funcoes e condicionais

---
+++ aula03-modulos-condicionais/aula03c-condicionais.py
@@ -0,0 +1,37 @@
+# ESTRUTURAS CONDICIONAIS
+
+nota_final = 4
+
+if nota_final < 6:
+    print("reprovado")
+else:
+    print("aprovado")
+
+print("FIM")
+
+# COMPOSTA
+nota_final = 7
+
+if nota_final < 6:
+    print("reprovado")
+else:
+    print ("aprovado")
+
+print("FIM")
+
+# ENCADEADA
+nota = 3.0
+if nota < 4:
+    print("reprovado")
+elif nota < 6:
+    print ("recuperação")
+else:
+    print("aprovado")
+
+
+
+
+
+
+
+
